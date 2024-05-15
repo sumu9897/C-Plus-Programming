d0c520695a92ae4d42555867880f9a2267161274
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-05-15 10:03:53 +0600

    Introduction to C++

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "w", stdin);
+        freopen("output.txt", "r", stdout);
+    #endif
+
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+
+
+    return 0;
+}
